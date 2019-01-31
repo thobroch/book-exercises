6623ac1ac5ceb2bae3a2aadec8fb4a9b5408e02d
Thomas <thobroch@hotmail.com>
2019-01-31 08:20:29 -0800

    Finish Chapter 11 exercise 6

--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -5,14 +5,21 @@
 #install.packages("nycflights13")  # should be done already
 library("nycflights13")
 library("dplyr")
-
+View(airports)
 # Create a dataframe of the average arrival delays for each _destination_, then 
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
+destination_group <- group_by(flights, dest)
+average_arr_delays_by_dest <- summarize(destination_group, mean_arr_delays = mean.default(arr_delay, na.rm = TRUE))
+left_join(average_arr_delays_by_dest, airports, by = "name")
 
+# San Francisco Airport
 
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+airline_group <- group_by(flights, carrier)
+average_arr_delays_by_airline <- summarize(airline_group, mean_arr_delays = mean.default(arr_delay, na.rm = TRUE))
+left_join(average_arr_delays_by_airline, airlines, by = "carrier")
+# Alaska Airlines
