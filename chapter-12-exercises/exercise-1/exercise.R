db4c1acc78b1c14738d1bb9704271d72c5b273b1
Thomas <thobroch@hotmail.com>
2019-01-31 19:08:51 -0800

    Correct Chapter 12 exercise 1

--- chapter-12-exercises/exercise-1/exercise.R
+++ chapter-12-exercises/exercise-1/exercise.R
@@ -1,33 +1,38 @@
 # Exercise 1: analyzing avocado sales with the `tidyr` package
 
 # Load necessary packages (`tidyr`, `dplyr`, and `ggplot2`)
-
-
+#install.packages("tidyr")
+#install.packages("ggplot2")
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 # Set your working directory using the RStudio menu:
 # Session > Set Working Directory > To Source File Location
 
 # Load the `data/avocado.csv` file into a variable `avocados`
 # Make sure strings are *not* read in as factors
-
+avocados <- read.csv("data/avocado.csv", stringsAsFactors = FALSE)
+View(avocados)
 
 # To tell R to treat the `Date` column as a date (not just a string)
 # Redefine that column as a date using the `as.Date()` function
 # (hint: use the `mutate` function)
+mutate(avocados, Date = as.Date(Date))
 
 
 # The file had some uninformative column names, so rename these columns:
 # `X4046` to `small_haas`
 # `X4225` to `large_haas`
 # `X4770` to `xlarge_haas`
-
+`colnames<-`(avocados, c("Date", "AveragePrice", "Total.Volume", "X4046" = "small_haas", "X4225" = "large_haas", "X4225" = "xlarge_haas" ))
 
 # The data only has sales for haas avocados. Create a new column `other_avos`
 # that is the Total.Volume minus all haas avocados (small, large, xlarge)
-
+mutate(avocados, other_avos = Total.Volume - sum(avocados$small_haas, avocados$large_haas, avocados$xlarge_haas))
 
 # To perform analysis by avocado size, create a dataframe `by_size` that has
 # only `Date`, `other_avos`, `small_haas`, `large_haas`, `xlarge_haas`
-
+by_size <- avocados[c("Date", "other_avos", "small_haas", "large_haas", "xlarge_haas") , ]
 
 # In order to visualize this data, it needs to be reshaped. The four columns
 # `other_avos`, `small_haas`, `large_haas`, `xlarge_haas` need to be 
@@ -36,11 +41,13 @@
 # `volume`. Create a new dataframe `size_gathered` by passing the `by_size` 
 # data frame to the `gather()` function. `size_gathered` will only have 3 
 # columns: `Date`, `size`, and `volume`.
-
+size_gathered <- gather(by_size)
+size_gathered
 
 # Using `size_gathered`, compute the average sales volume of each size 
 # (hint, first `group_by` size, then compute using `summarize`)
-
+group_by(size_gathered, size)
+summarize(size_gathered, mean.default(size, na.rm = TRUE ))
 
 # This shape also facilitates the visualization of sales over time
 # (how to write this code is covered in Chapter 16)
@@ -52,13 +59,14 @@ ggplot(size_gathered) +
 # Create a new data frame `by_type` by grouping the `avocados` dataframe by
 # `Date` and `type`, and calculating the sum of the `Total.Volume` for that type
 # in that week (resulting in a data frame with 2 rows per week).
-
+by_type <- group_by(avocados, Date, type)
+summarize(by_type, sum_Total = sum(Total.Volume))
 
 # To make a (visual) comparison of conventional versus organic sales, you 
 # need to **spread** out the `type` column into two different columns. Create a 
 # new data frame `by_type_wide` by passing the `by_type` data frame to 
 # the `spread()` function!
-
+by_type_wide <- spread(by_type, type)
 
 # Now you can create a scatterplot comparing conventional to organic sales!
 # (how to write this code is covered in Chapter 16)
